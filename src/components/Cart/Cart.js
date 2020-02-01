25cac52ec883c778acf668d9f627ea72d1da3144
Dima <zahrebelny@gmail.com>
2020-02-01 23:31:58 +0200

    refactoring

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Products from './Products/Product';
 import Sidebar from './Sidebar/Sidebar';
