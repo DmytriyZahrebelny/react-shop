ba719fb5560bc0bf7931b907b7c941e5cfb415c9
Dima <zahrebelny@gmail.com>
2020-02-01 21:06:44 +0200

    update page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Products from './Product';
 import Sidebar from './Sidebar';
@@ -19,48 +19,39 @@ const ListProducts = styled.ul`
 	padding: 0;
 `;
 
-class Cart extends Component {
-	render() {
-		const {
-			products: { products },
-			selectedProduct: { productsId },
-		} = this.props;
-
-		const quantityProducts = productsId.reduce((acc, id) => {
-			acc[id] = acc[id] ? acc[id] + 1 : 1;
-			return acc;
-		}, {});
-
-		const allPrice = products.reduce((sum, product) => {
-			if (quantityProducts[product.id]) {
-				return sum + quantityProducts[product.id] * product.price;
-			}
-			return sum;
-		}, 0);
-
-		return (
-			<form onSubmit={evt => evt.preventDefault()} action='#'>
-				<CartContainer>
-					<ListProducts>
-						{Object.keys(quantityProducts)
-							.map(id => products.filter(product => product.id === id))
-							.reduce((acc, arr) => [...acc, ...arr], [])
-							.map(product => (
-								<Products key={product.id} product={product} quantityProducts={quantityProducts} />
-							))}
-					</ListProducts>
-					<Sidebar allPrice={allPrice} />
-				</CartContainer>
-			</form>
-		);
-	}
-}
-
-const mapStateToProps = state => {
-	return {
+const Cart = () => {
+	const { products, selectedProduct } = useSelector(state => ({
 		products: state.productsReducer,
 		selectedProduct: state.cartReducer,
-	};
+	}));
+
+	const quantityProducts = selectedProduct.productsId.reduce((acc, id) => {
+		acc[id] = acc[id] ? acc[id] + 1 : 1;
+		return acc;
+	}, {});
+
+	const allPrice = products.products.reduce((sum, product) => {
+		if (quantityProducts[product.id]) {
+			return sum + quantityProducts[product.id] * product.price;
+		}
+		return sum;
+	}, 0);
+
+	return (
+		<form onSubmit={evt => evt.preventDefault()} action='#'>
+			<CartContainer>
+				<ListProducts>
+					{Object.keys(quantityProducts)
+						.map(id => products.products.filter(product => product.id === id))
+						.reduce((acc, arr) => [...acc, ...arr], [])
+						.map(product => (
+							<Products key={product.id} product={product} quantityProducts={quantityProducts} />
+						))}
+				</ListProducts>
+				<Sidebar allPrice={allPrice} />
+			</CartContainer>
+		</form>
+	);
 };
 
-export default connect(mapStateToProps)(Cart);
+export default Cart;
